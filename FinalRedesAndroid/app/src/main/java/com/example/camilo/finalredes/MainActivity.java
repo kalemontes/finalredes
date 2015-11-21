5d9cd36e3fd94dd8154f1db9e008aafbbe516b8b
Camilo Montes <ale8689@gmail.com>
2015-11-21 20:47:12 +0100

    Creamos el splashscreen de la app

---
+++ FinalRedesAndroid/app/src/main/java/com/example/camilo/finalredes/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.camilo.finalredes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MainActivity extends AppCompatActivity implements Observer {
+    private Button bnPlay;
+    private Button bnQuit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Comunicacion.getInstance().addObserver(this);
+
+        bnPlay = (Button) findViewById(R.id.bnPlay);
+        bnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentFullScreen = new Intent(getApplicationContext(), FullscreenActivity.class);
+                startActivity(intentFullScreen);
+            }
+        });
+
+        bnQuit = (Button) findViewById(R.id.bnQuit);
+        bnQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void update(Observable observable, Object data) {
+        Log.i("MainActivity", data.toString());
+    }
+}
