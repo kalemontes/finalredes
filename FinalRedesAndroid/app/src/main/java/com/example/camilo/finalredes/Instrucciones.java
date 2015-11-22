a83d7a57628929154061800ab838735344d452f2
danieldp1990 <dandopa@gmail.com>
2015-11-22 16:19:16 -0500

    se agregan los botones de cerdokiller
    se agregan los botones de los pajaron para disparar

--- FinalRedesAndroid/app/src/main/java/com/example/camilo/finalredes/Instrucciones.java
+++ FinalRedesAndroid/app/src/main/java/com/example/camilo/finalredes/Instrucciones.java
@@ -2,12 +2,33 @@ package com.example.camilo.finalredes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Instrucciones extends AppCompatActivity {
+    private ImageView ImgInstruccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrucciones);
+        ImgInstruccion = (ImageView) findViewById(R.id.imgInstrucciones);
+
+        String jugador = getIntent().getStringExtra("JUGADOR");
+        switch (jugador) {
+            case "ROL_JUGADOR:EMPOLLADOR":
+                ImgInstruccion.setImageResource(R.drawable.instruccion3);
+                break;
+            case "ROL_JUGADOR:CERDOKILLER":
+                ImgInstruccion.setImageResource(R.drawable.instruccion1);
+                break;
+            case "ROL_JUGADOR:MONJA":
+                ImgInstruccion.setImageResource(R.drawable.instruccion2);
+                break;
+
+        }
     }
 }
+
