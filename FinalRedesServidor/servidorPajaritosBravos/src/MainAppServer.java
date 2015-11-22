13d4afce2eb1d0a7ae9465a36f9f1cfe1c626e94
danielmontesg <danielmontesfreesound@gmail.com>
2015-11-21 21:22:12 -0500

    Utilizacion de Processin para dibujar, nueva carpeta data con imagenes + pintada de escenario

--- FinalRedesServidor/servidorPajaritosBravos/src/MainAppServer.java
+++ FinalRedesServidor/servidorPajaritosBravos/src/MainAppServer.java
@@ -1,8 +1,17 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 
 public class MainAppServer extends PApplet{	
-	Servidor server;	
+	Servidor server;
+	
+	private PApplet applet;
+	private PImage escenario, nido, rojo, azul, amarillo, cauchera, cerdo, cerdo2, cerdo3, huevo; 
+	
+
+	
+
+	
 	@Override
 	public void setup() {		
 		server = new Servidor(this);
@@ -10,5 +19,15 @@ public class MainAppServer extends PApplet{
 		
 		size(1080,720);
 	    background(0);
+	    
+		escenario = loadImage("../data/escenario.png");
+//		rojo = loadImage("rojo.png");
+//		azul= loadImage("azul.png");
+//		amarillo= loadImage("amarillo.png");
+//		cerdo = loadImage("cerdo.png");
+//		loadImage("");
+	}
+	public void draw(){
+		image(escenario,0,0);
 	}
 }
