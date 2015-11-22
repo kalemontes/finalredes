df00293d1a5de5a66c0aac40e7afab04611c6faf
Camilo Montes <ale8689@gmail.com>
2015-11-22 22:28:25 +0100

    Los huevos se dibujan segun el centro de su imagen

--- FinalRedesServidor/servidorPajaritosBravos/src/Huevo.java
+++ FinalRedesServidor/servidorPajaritosBravos/src/Huevo.java
@@ -2,7 +2,11 @@
 public class Huevo extends Drawable {
 
 	public Huevo(MainAppServer p, int initX, int initY) {
-		super(p, p.huevo, initX - 21, initY - 53); //el huevo se posiciona desde la parte central abajo
+		super(p, p.huevo, initX, initY); 
 	}
 	
+	@Override
+	public void display() {
+		parent.image(img, xCenter, yCenter);//el huevo se posiciona segun el centro de la imagen
+	}
 }
