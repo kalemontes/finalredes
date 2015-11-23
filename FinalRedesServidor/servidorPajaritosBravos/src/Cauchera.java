42fa727b0b47ac3940150e240e18c81ac073fc77
danielmontesg <danielmontesfreesound@gmail.com>
2015-11-23 18:22:26 -0500

    Lanzamiento de Rojo + Cauchera + Empollador entrega 1 huevo

---
+++ FinalRedesServidor/servidorPajaritosBravos/src/Cauchera.java
@@ -0,0 +1,9 @@
+
+public class Cauchera extends Drawable {
+
+	public Cauchera(MainAppServer p, int initX, int initY) {
+		super(p, p.cauchera, initX, initY);
+		// TODO Auto-generated constructor stub
+	}
+
+}
