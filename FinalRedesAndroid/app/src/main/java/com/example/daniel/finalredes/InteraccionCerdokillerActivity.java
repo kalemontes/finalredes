950e0c02a27039f416dd355dae4307ade98968f6
danieldp1990 <dandopa@gmail.com>
2015-11-22 19:22:08 -0500

    cambia botones play con especificacion.
    se le agrega especificacion a los botones

--- FinalRedesAndroid/app/src/main/java/com/example/daniel/finalredes/InteraccionCerdokillerActivity.java
+++ FinalRedesAndroid/app/src/main/java/com/example/daniel/finalredes/InteraccionCerdokillerActivity.java
@@ -2,11 +2,14 @@ package com.example.daniel.finalredes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 /**
  * Created by Daniel on 22/11/2015.
  */
 public class InteraccionCerdokillerActivity extends AppCompatActivity {
+    private ImageView bnPlay,bnPlay2,bnPlay3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
